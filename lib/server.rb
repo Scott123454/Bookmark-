35bb48d1a778b129dbaa86cd0d17dfe833decb4f
Scott123454 <scott123454@hotmail.co.uk>
2014-05-08 14:36:48 +0100

    links with tags

--- lib/server.rb
+++ lib/server.rb
@@ -2,10 +2,12 @@
 env = ENV["RACK_ENV"] || "development"
 require 'data_mapper'
 require 'sinatra'
+
 # we're telling datamapper to use a postgres database on localhost. The name will be "bookmark_manager_test" or "bookmark_manager_development" depending on the environment
 DataMapper.setup(:default, "postgres://localhost/bookmark_manager_#{env}")
 
 require './lib/link' # this needs to be done after datamapper is initialised
+require './lib/tag'
 
 # After declaring your models, you should finalise them
 DataMapper.finalize
@@ -21,9 +23,16 @@ end
 post '/links' do
   url = params["url"]
   title = params["title"]
-  Link.create(:url => url, :title => title)
+  tags = params["tags"].split(" ").map do |tag|
+  Tag.first_or_create(:text => tag)
+  end
+  Link.create(:url => url, :title => title, :tags => tags)
   redirect to('/')
 end
 
-
+get '/tags/:text' do
+  tag = Tag.first(:text => params[:text])
+  @links = tag ? tag.links : []
+  erb :index
+end
 
